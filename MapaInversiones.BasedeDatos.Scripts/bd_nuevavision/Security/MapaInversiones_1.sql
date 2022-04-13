76e8ffb014aec81411c87ed020734e2bffde8f93
afvillamizar <villamizarvecino@hotmail.com>
2022-04-12 21:25:19 -0500

    Scripts Base de Datos

---
+++ MapaInversiones.BasedeDatos.Scripts/bd_nuevavision/Security/MapaInversiones_1.sql
@@ -0,0 +1,3 @@
+﻿CREATE LOGIN [MapaInversiones]
+    WITH PASSWORD = N'7jyaptdqisi***REMOVED***~zfMdp|axyhnmsFT7_&#$!~<***REMOVED***opkdoxojpnc', SID = 0x213B728A7D70DA4EAE3EC50E6DA5ABB4, DEFAULT_LANGUAGE = [Español], CHECK_POLICY = OFF;
+
